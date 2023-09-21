799d5ca5fdc061608d5476b8a06f0bcaa2b143da
malabasterr <113557081+malabasterr@users.noreply.github.com>
2023-09-21 15:49:45 +0100

    add buttons, change theme

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import './HomePage.css';
 
 function HomePage() {
   return (
-    <div>HomePage</div>
+    <>
+      <div>HomePage</div>
+      <div className='claimRouteButtonContainer'>
+        <button className="claimRouteButton">CLAIM A ROUTE</button>
+      </div>
+      <div className='unclaimRouteButtonContainer'>
+        <button className="unclaimRouteButton">UNCLAIM A ROUTE</button>
+      </div>
+      <div className='claimBonusSiteButtonContainer'>
+        <button className="claimBonusSiteButton">CLAIM A BONUS SITE</button>
+      </div>
+      <div className='shareLocationButtonContainer'>
+        <button className="shareLocationButton">SHARE LOCATION</button>
+      </div>
+      <div className='drawScrewYouCardButtonContainer'>
+        <button className="drawScrewYouCardButton">DRAW SCREW YOU CARD</button>
+      </div>
+    </>
   )
 }
 
