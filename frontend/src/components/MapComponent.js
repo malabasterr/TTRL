b0fce486e52ea8676cd402b30f2285bc539282ef
malabasterr <113557081+malabasterr@users.noreply.github.com>
2023-10-02 11:09:41 +0100

    integrate api cities and routes on map

--- frontend/src/components/MapComponent.js
+++ frontend/src/components/MapComponent.js
@@ -3,52 +3,70 @@ import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 
 class MapComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cityData: [],
+      connectionData: [],
+    };
+  }
+
   componentDidMount() {
     const container = L.DomUtil.get('map');
     if (!container._leaflet_id) {
-      const map = L.map('map').setView([48.505, 13.09], 4); // Set initial coordinates and zoom level
+      const map = L.map('map').setView([48.505, 13.09], 4);
       L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
-      fetch('CITY-API-URL') // Ask Will to create an endpoint of all cities
+      fetch('/cities/')
         .then((response) => response.json())
         .then((cityData) => {
           this.setState({ cityData });
 
-          fetch('http://localhost:8000/routes/')
+          fetch('/routes/')
             .then((response) => response.json())
             .then((connectionData) => {
               this.setState({ connectionData });
 
               cityData.forEach((city) => {
                 const marker = L.marker([city.latitude, city.longitude]).addTo(map);
-                marker.bindPopup(city.name); // Display city name when the marker is clicked
+                marker.bindPopup(city.name);
               });
 
+              // Loop through each route in the connectionData
               connectionData.forEach((connection) => {
-                const sourceCity = this.state.cityData.find((city) => city.id === connection.sourceCityId);
-                const destinationCity = this.state.cityData.find((city) => city.id === connection.destinationCityId);
+                // Find the source city in the cityData using the start_city_id from the route
+                const sourceCity = this.state.cityData.find((city) => city.id === connection.start_city_id);
 
+                // Find the destination city in the cityData using the end_city_id from the route
+                const destinationCity = this.state.cityData.find((city) => city.id === connection.end_city_id);
+
+                // Check if both source and destination cities are found
                 if (sourceCity && destinationCity) {
+                  // Create an array of coordinates for the polyline between source and destination
                   const coordinates = [
-                    [sourceCity.latitude, sourceCity.longitude],
-                    [destinationCity.latitude, destinationCity.longitude],
+                    [sourceCity.latitude, sourceCity.longitude], // Source city coordinates
+                    [destinationCity.latitude, destinationCity.longitude], // Destination city coordinates
                   ];
 
+                  // Log source and destination cities to check if they are correctly identified
+                  console.log('Source City:', sourceCity);
+                  console.log('Destination City:', destinationCity);
+
                   // Draw the polyline on the map with specified style
-                  L.polyline(coordinates, { color: 'blue', weight: 2 }).addTo(this.map);
+                  L.polyline(coordinates, { color: 'blue', weight: 2 }).addTo(map);
+                } else {
+                  // Log a warning if either source or destination city is not found
+                  console.warn('Source or destination city not found for connection:', connection);
                 }
               });
             })
             .catch((error) => {
+              // Handle any errors that occur during the fetch operation
               console.error('Error fetching connection data:', error);
             });
         })
-        .catch((error) => {
-          console.error('Error fetching city data:', error);
-        });
     }
   }
-  
   render() {
     return <div id="map" style={{ height: '35vh' }}></div>;
   }
